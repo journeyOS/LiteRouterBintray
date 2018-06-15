9b86809717d4afb1b4df8680d1eba8632a542619
Solo <anqi.huang@outlook.com>
2018-06-15 15:28:00 +0800

    This poject use to update literouter to bintray
    Signed-off-by: Solo <anqi.huang@outlook.com>

---
+++ app/src/com/journeyOS/router/RouterActivity.java
@@ -0,0 +1,12 @@
+package com.journeyOS.router;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RouterActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.router_activity);
+    }
+}
